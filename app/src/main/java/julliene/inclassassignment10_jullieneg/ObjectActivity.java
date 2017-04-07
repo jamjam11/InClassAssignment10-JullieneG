6861a8db791da3f3562e07b8c8e7c8e7cf14efd6
jamjam11 <jmg2307@tc.columbia.edu>
2017-04-06 20:50:34 -0400

    Initial commit

---
+++ app/src/main/java/julliene/inclassassignment10_jullieneg/ObjectActivity.java
@@ -0,0 +1,13 @@
+package julliene.inclassassignment10_jullieneg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ObjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_object);
+    }
+}
